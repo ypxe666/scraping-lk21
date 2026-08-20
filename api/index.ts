171bf6450db7b5eb2f1f3b5df9be37b96d298971
spamtelur <limagorosei05@gmail.com>
2026-08-20 07:12:13 +0700

    Rebuiilt

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,11 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-import express, {
-  type Application,
-  type Request,
-  type Response,
-} from "express";
+import express, { type Application, type Request, type Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
-import routes from "./routes/routes.js";
+import routes from "../src/routes/routes.js";
+
 const app: Application = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -17,8 +13,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({ origin: "*" }));
 
-app.get("/", (request: Request, response: Response) => {
-  response.json({ message: "Berhasil Terhubung!" });
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ message: "Berhasil Terhubung!" });
 });
 
 app.use(routes);
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -25,19 +25,19 @@ const routes: IRouter = Router();
 routes.get("/movies/latest", latestMovie);
 routes.get("/movies/rating", ratingMovies);
 routes.get("/movies/populer", populerMovies);
-routes.get("/movies/:id", detailMovie);
-routes.get("/movies/:id/stream", streamMovie);
 routes.get("/movies/genre/:param", genreMovie);
 routes.get("/movies/country/:param", countryMovie);
+routes.get("/movies/:id/stream", streamMovie);
+routes.get("/movies/:id", detailMovie);
 
 // series
 routes.get("/series/latest", latestSeries);
 routes.get("/series/populer", populerSeries);
 routes.get("/series/rating", ratingSeries);
 routes.get("/series/ongoing", ongoingSeries);
-routes.get("/series/:id", detailSeries);
-routes.get("/series/:id/stream", streamSeries);
 routes.get("/series/genre/:param", genreSeries);
 routes.get("/series/country/:param", countryseries);
+routes.get("/series/:id/stream", streamSeries);
+routes.get("/series/:id", detailSeries);
 
 export default routes;
